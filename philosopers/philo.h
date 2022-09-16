b7b3ec7db88e92dabf3ba50d72c0aa6e3e1f0860
Fabio Rudello <frudello@e4r5p2.42roma.it>
2022-09-16 16:03:40 +0200

    pipposolococaina

---
+++ philosopers/philo.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: frudello <frudello@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/16 15:44:26 by frudello          #+#    #+#             */
+/*   Updated: 2022/09/16 16:02:29 by frudello         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <limits.h>
+# include <sys/time.h>
+# include <pthread.h>
+
+# define EAT "is eating"
+# define SLEEP "is sleeping"
+# define THINK "is thinking"
+# define FORK "has taken a fork"
+# define DIED "died ☠️"
+
+
+
+#endif
